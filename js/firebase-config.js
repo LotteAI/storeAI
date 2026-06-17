9a8b31a884c7f689d97afe42b0899dd4126e48ce
LotteAI <lotteaxteam@gmail.com>
2026-06-17 11:22:05 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,7 +6,7 @@
  */
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiRQ-GHT3jsseTA85U9m9proepfLQsQ9Y",
+  apiKey: 
   authDomain: "store-ai-ec4d5.firebaseapp.com",
   projectId: "store-ai-ec4d5",
   storageBucket: "store-ai-ec4d5.firebasestorage.app",
